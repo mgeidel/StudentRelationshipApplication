6cd36dd0aaf4c350db14680e01d4e75a479a6f3d
mac <mac@MacsBook-Air.local>
2013-08-09 17:17:25 +0200

    Initial Commit

---
+++ StudentRelationshipApplication/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StudentRelationshipApplication
+//
+//  Created by mac on 09.08.13.
+//  Copyright (c) 2013 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
